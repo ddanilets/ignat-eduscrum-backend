009ae9ab0eea5f6b7ebefa7294a5af7cbb185c24
ddanilets <ddanilets@gmail.com>
2017-03-12 01:29:15 +0200

    add host

--- ignat/settings.py
+++ ignat/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '2&4)0gdvza&ex4&p^xwrt1s7s1dox3-j+*qg7c7z-ub(92cdx6'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['ignat-eduscrum.herokuapp.com',]
 
 
 # Application definition
