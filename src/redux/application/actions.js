dfbcafae86c6a506d8f2915b180a483a16b0ffd5
Scanners87 <33575006+Scanners87@users.noreply.github.com>
2017-11-11 15:01:21 +0000

    Fixed two typos
    Fixed two typos of language

--- src/redux/application/actions.js
+++ src/redux/application/actions.js
@@ -1,6 +1,6 @@
 /* global VK*/
 import * as constants from './constants';
 
-export function changeLanguage(langauge) {
-  return { type: constants.CHANGE_LANGUAGE, payload: langauge };
+export function changeLanguage(language) {
+  return { type: constants.CHANGE_LANGUAGE, payload: language };
 }
